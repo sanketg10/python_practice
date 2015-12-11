2cdf2f63c4c4ef190fcd54e23d52c85675581a5e
Sanket Gupta <sg3184@columbia.edu>
2015-12-10 23:57:14 -0500

    improved the palindrome demo and now can display maximum value per se

--- palindrome_try2.py
+++ palindrome_try2.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math  
 
-#To calculate the largest palindrome among all products of 3-digit numbers
+#To calculate the largest palindrome among all products of 3-digit numbers 
+r = 1
+max = 0
 for f in range(1,1000,1): 
 	for k in range(1,1000,1): 
 		n = f * k 
@@ -11,15 +13,27 @@ for f in range(1,1000,1):
 		#num2 = [100000] * 100 #To hold all the palindrome values 
 		i = 0  
 		count = 0
-		j = 0
+		j = 0 
 		while n: 
 			#print(n %10) 
 			num[i] = n%10 
 			n /= 10 
-			i = i + 1 
-		for i in range(0,3):
-			if num[i] == num[5-i]:  
-				count = count + 1  
+			i = i + 1  
+
+		if i >5: 
+			for i in range(0,3):
+				if num[i] == num[5-i]:  
+					count = count + 1  
 					#print ( f, k , f*k, count)
-			if count>2: 
-				print "This is a palindrome of", f, "times", k ,"which is", f*k
+				if count>2: 
+					print "This is a palindrome", r, "of", f, "times", k ,"which is", f*k
+					r = r + 1
+					if f*k > max: 
+						max= f*k 
+			
+print "And the winner is...." 
+print "........Maximum palindrome we are looking for is", max
+
+
+
+
